4c4280515f10e2e07cac6d992d683e9cabf4e809
Alnaggar94 <49800146+Alnaggar94@users.noreply.github.com>
2021-11-07 00:33:19 +0300

    Update composer.sh

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -5,3 +5,9 @@ cp _install-files/_wp-silencer.php assets/themes/index.php
 
 ## Redis Object Cache
 cp assets/mu-plugins/litespeed-cache/lib/object-cache.php assets/drop-ins
+
+
+## Change imptrnat file permssion 
+sudo chmod 400 app/wp-config.php
+sudo chmod 400 config/wp-constants.php
+sudo chmod 400 config/.htaccess-standard
